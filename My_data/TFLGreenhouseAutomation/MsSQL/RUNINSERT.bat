e42324773135caa7160b0810d503f5756555f90f
KaustubhPatil10 <kaustubhpatil331@gmail.com>
2024-10-24 13:44:42 +0530

    added task scheduler

---
+++ My_data/TFLGreenhouseAutomation/MsSQL/RUNINSERT.bat
@@ -0,0 +1,1 @@
+sqlcmd -S ADITYA\SQLEXPRESS -d TFLGreenhouseAutomation -E -i "C:\Users\adity\OneDrive\Desktop\kaustubh data\TAP\Ravi_sir_MENTOR\src\My_data\TFLGreenhouseAutomation\MsSQL\sampledb1.sql"
---
+++ My_data/TFLGreenhouseAutomation/MsSQL/sampledb1.sql
@@ -0,0 +1,3 @@
+USE TFLGreenhouseAutomation;
+INSERT INTO SensorReadings (SensorID, ReadingValue, ReadingTime) VALUES 
+(1, 25.5, GETDATE());  -- Temperature reading
