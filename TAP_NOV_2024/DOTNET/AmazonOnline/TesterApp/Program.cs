ebeb483fee02feb1996a1ebd4debbe0f63f71a59
KaustubhPatil10 <kaustubhpatil331@gmail.com>
2024-11-11 17:44:16 +0530

    added new AmazonOnline for dotnet

---
+++ TAP_NOV_2024/DOTNET/AmazonOnline/TesterApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Hello World ! " + args[i]);
+            }
+
+            //to give argument go to TestApp properties / under debug /write on command line 
+            Console.WriteLine("Welcome to DotNet Programming");
+            int count = 45;
+            count = count++;
+
+
+            if(count <= 300)
+            {
+                while(count < 299)
+                {
+                    Console.WriteLine("count = {0}", count);
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Please enter your name: ");
+            String name = Console.ReadLine();
+            Console.WriteLine("Good Morning {0}", name + " Patil");  //{}place holder
+
+            Program theprogram = new Program();
+            theprogram.Display();
+            Console.ReadLine();
+
+        }
+
+
+        static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Multiplication(int a , int b)
+        {
+            return a * b;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Processing Logic for Display");
+            Console.WriteLine("Program instance Dashboard");
+        }
+    }
+}
