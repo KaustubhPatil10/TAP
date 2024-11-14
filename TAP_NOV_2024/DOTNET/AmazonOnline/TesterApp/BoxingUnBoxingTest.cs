3932a281b703b30b9f3099b7df5aeb62bb011707
KaustubhPatil10 <kaustubhpatil331@gmail.com>
2024-11-14 17:30:24 +0530

    new changes using enum

---
+++ TAP_NOV_2024/DOTNET/AmazonOnline/TesterApp/BoxingUnBoxingTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesterApp
+{
+    class BoxingUnBoxingTest
+    {
+        static void Main(string [] args)
+        {
+            // value type
+            int count = 45;
+
+            //reference type - type of o is a class
+            //Boxing is a technique where value type are transformed into reference type
+            // Value type is wrapped in Reference Type
+
+            Object o = count;
+            //Console.Write(o);
+
+            //unboxing is a technique where reference is unwrapped and value type is received.
+            int result = (int) o;
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TAP_NOV_2024/DOTNET/AmazonOnline/TesterApp/constreadonlyTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TesterApp
+{
+    enum Weekdays { Mon, Tue, Wed, Thur, Fri, Sat, Sun};
+
+    enum Months { Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec};
+
+    enum FavouriteColor { Violet, Indigo, Blue, Green, Yellow, Orange, Red};
+    class MathEngine
+    {
+        public const int count = 56; // at the time of declaration you have to initialize.
+        public readonly double PI; //you can declare but no need to initialize here.
+
+        public MathEngine(int num1, int num2)
+        {
+            PI = 7.14;
+        }
+
+        public MathEngine()
+        {
+            PI = 3.14; // initialized only once.
+        }
+
+        public int Add(int op1, int op2)
+        {
+            //PI = 45;
+            return op1 + op2;
+        }
+
+    }
+    class constreadonlyTest
+    {
+        static void Main(string [] args)
+        {
+            Weekdays day = Weekdays.Mon;
+            Months month = Months.May;
+            FavouriteColor myColor = FavouriteColor.Red;
+
+            int[] schoolMarks;
+            int[] marks = new int[9];
+            int[] enggMarks = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] dacMarks = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            
+            
+            
+            Console.WriteLine(" {0}", day);
+            Console.WriteLine(" {0}", month);
+            Console.WriteLine(" {0}", myColor);
+
+            Console.ReadLine();
+        }
+    }
+}
