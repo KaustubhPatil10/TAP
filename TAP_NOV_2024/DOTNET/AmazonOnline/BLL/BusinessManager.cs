9264b6c41352acc81c37996b23bb7721c59bf078
KaustubhPatil10 <kaustubhpatil331@gmail.com>
2024-11-26 20:49:14 +0530

    BLL

---
+++ TAP_NOV_2024/DOTNET/AmazonOnline/BLL/BusinessManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Catalog;
+
+namespace BLL
+{
+    public static class BusinessManager
+    {
+        public static List<Product> GetAllProducts()
+        {
+
+        }
+    }
+}
