2e561c50f5b47e413567e02f346ae2cccd669a67
KaustubhPatil10 <kaustubhpatil331@gmail.com>
2024-11-27 19:11:48 +0530

    adding BLL- class Library, LINQTest App using IEnumberable

--- TAP_NOV_2024/DOTNET/AmazonOnline/BLL/BusinessManager.cs
+++ TAP_NOV_2024/DOTNET/AmazonOnline/BLL/BusinessManager.cs
@@ -11,7 +11,41 @@ namespace BLL
     {
         public static List<Product> GetAllProducts()
         {
+            List<Product> allProducts = new List<Product>();
+            allProducts.Add(new Product { ID = 1, Title = "Gerbera", Description = "Wedding FLowers", UnitPrice = 6,  Quantity = 5000 });
+            allProducts.Add(new Product { ID = 2, Title = "Rose", Description = "Valentine FLowers", UnitPrice = 6, Quantity = 7000 });
+            allProducts.Add(new Product { ID = 3, Title = "Lotus", Description = "Worship FLowers", UnitPrice = 6, Quantity = 0 });
+            allProducts.Add(new Product { ID = 4, Title = "Carnation", Description = "Pink carnation signifies a Mother's Lover", UnitPrice = 6, Quantity = 27000 });
+            allProducts.Add(new Product { ID = 5, Title = "Lily", Description = "Lilies are among the most popular flowers in the U.S", UnitPrice = 6, Quantity = 1000 });
+            allProducts.Add(new Product { ID = 6, Title = "Jasmine", Description = "Jasmine is a genus pf shrubs and vines in the olive family", UnitPrice = 6, Quantity = 0 });
+            allProducts.Add(new Product { ID = 7, Title = "Daisy", Description = "Give a gift of these cheerful flower as a symbol of your Loyalty and pure intentions", UnitPrice = 6, Quantity = 159 });
+            allProducts.Add(new Product { ID = 8, Title = "Aster", Description = "Asters are the september birth flowers", UnitPrice = 6, Quantity = 67 });
+            allProducts.Add(new Product { ID = 9, Title = "Daffodil", Description = "Wedding flower", UnitPrice = 6, Quantity = 5000 });
+            allProducts.Add(new Product { ID = 10, Title = "Dahlia", Description = "Dahlias are a popular and glamorous summer flower", UnitPrice = 6, Quantity = 0 });
+            allProducts.Add(new Product { ID = 11, Title = "Hydrangea", Description = "Hydrangea is the fourth wedding anniversary flower", UnitPrice = 6, Quantity = 0 });
+            allProducts.Add(new Product { ID = 12, Title = "Orchid", Description = "Orchids are exotic and beautiful making a perfect bouquet", UnitPrice = 6, Quantity = 700 });
+            allProducts.Add(new Product { ID = 13, Title = "Statice", Description = "Surprise them with this fresh, fabulous array of Static flowers", UnitPrice = 6, Quantity = 1500 });
+            allProducts.Add(new Product { ID = 14, Title = "Sunflower", Description = "Sunflowers express your pure love", UnitPrice = 6, Quantity = 2500 });
+            allProducts.Add(new Product { ID = 15, Title = "Tulip", Description = "Tulips are the quintessential spring flower and available from January", UnitPrice = 6, Quantity = 10000 });
 
+            return allProducts;
+        }
+
+        public static IEnumerable<Product> GetSoldOutProducts()
+        {
+            List<Product> products = GetAllProducts();
+            // apply filter
+            // apply some business query
+            // var is used to represent a Dynamic Variable.
+            // where the type will be known at Run Time and not on Compile Time. 
+            // var is known as a Data Type.
+            // LINQ: Language Intergated Query
+            // var soldOutProducts = from product in products select product;
+            var soldOutProducts = from product in products
+                                     where product.Quantity == 0
+                                     select product;
+            return soldOutProducts;
+            // return products;
         }
     }
 }
---
+++ TAP_NOV_2024/DOTNET/AmazonOnline/TesterApp/LINQTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BLL;
+using Catalog;
+namespace TesterApp
+{
+    public class LINQTest
+    {
+        // Will act like console UI
+        static void Main(String[] args)
+        {
+            List<Product> allProducts  = BusinessManager.GetAllProducts();
+
+
+            List<int> numbers = new List<int>() { 5,4,1,3,9,8,6,7,2,0};
+
+
+            // The query variable can also be implicitly typed by using var
+            // Query #1.
+            IEnumerable<int> filteringQuery = from num in numbers
+                                              where num < 3 || num > 7
+                                              select num;
+
+            Console.WriteLine("Show number which are not in between 3 and 7");
+            foreach(int score in filteringQuery)
+            {
+                Console.WriteLine(score);
+            }
+
+            // Query #2.
+            IEnumerable<int> orderingQuery = from num in numbers
+                                             where num < 3 || num > 7
+                                             orderby num descending
+                                             select num;
+
+            Console.WriteLine("Show number which are not in between 3 and 7 in descending manner");
+            foreach(int score in orderingQuery)
+            {
+                Console.WriteLine(score);
+            }
+
+
+
+
+
+
+
+
+            // instead of  var we can also write IEnumerable<Product> .
+
+            /*var allSoldOutProducts = from product in allProducts
+                                                      where product.Quantity == 0
+                                                      select product; */
+
+            IEnumerable < Product > allSoldOutProducts = BusinessManager.GetSoldOutProducts();
+            Console.WriteLine(" Show only those product title which are sold out");
+            
+            foreach (Product theProduct in allSoldOutProducts)
+            {
+                Console.WriteLine(theProduct.Title + " " + theProduct.Quantity);
+            } 
+            Console.ReadLine();
+        }
+    }
+}
